4cfc6a164b8eddc7a697e59a32e8af889dca797e
Luis Martín <luismartingarcia4@gmail.com>
2025-04-14 16:49:52 +0200

    module 01 done

---
+++ cpp/module_01/ex06/Harl.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lumartin <lumartin@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 16:38:00 by lumartin          #+#    #+#             */
+/*   Updated: 2025/04/14 16:45:56 by lumartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+private:
+    std::string level;
+    void debug();
+    void info();
+    void warning();
+    void error();
+
+public:
+    Harl();
+    ~Harl();
+    void complain(std::string level);
+};
+
+#endif
