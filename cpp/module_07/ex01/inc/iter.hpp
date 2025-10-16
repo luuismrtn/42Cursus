f10b64d8ecddb390ca735729a3e603e6b41ca15d
Luis Martin Garcia <lumartin@c2r16s2.42madrid.com>
2025-10-16 16:55:09 +0200

    CPP 07 100%

--- cpp/module_07/ex01/inc/iter.hpp
+++ cpp/module_07/ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: lumartin <lumartin@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 18:25:47 by lumartin          #+#    #+#             */
-/*   Updated: 2025/08/24 18:35:31 by lumartin         ###   ########.fr       */
+/*   Updated: 2025/10/16 16:52:37 by lumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,16 @@
 #include <iostream>
 
 template <typename T>
-void iter(T *array, std::size_t length, void (*func)(T &))
+void iter(T *array, const std::size_t length, void (*func)(const T &))
+{
+    for (std::size_t i = 0; i < length; ++i)
+    {
+        func(array[i]);
+    }
+}
+
+template <typename T>
+void iter(T *array, const std::size_t length, void (*func)(T &))
 {
     for (std::size_t i = 0; i < length; ++i)
     {
